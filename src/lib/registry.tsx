eedd46fc1ead029b25cd130cb2d91ce122bddf1c
Patrick Tavares <patrick.t2@hotmail.com>
2025-09-16 16:57:05 -0300

    alteração

---
+++ src/lib/registry.tsx
@@ -0,0 +1,24 @@
+// lib/registry.tsx
+"use client";
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+export default function StyledComponentsRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = sheet.getStyleElement();
+    sheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") return <>{children}</>;
+
+  return <StyleSheetManager sheet={sheet.instance}>{children}</StyleSheetManager>;
+}
